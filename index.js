b98818eade4f7a114de4fad557ed646b45b7067a
minhajspace <minhaj.space@gmail.com>
2020-11-18 16:52:51 +0530

    file base module

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
-// const squre =require('./math')
+const squre =require('./math')
 
-// const calsqure =(a)=>{
-//     console.log(`the value of a is ${a} and the area is ${squre.area(a)} and the parameter is ${squre.parameters(a)}` )
-// }
-
-// calsqure(5)
-
-
-const arrayOne= [1,2,34,56,7,8,9];
-const arrayTow =[2,4,35,6,7]
-
-function findCommonArray (){
-    arrayOne.map((value)=>{
-    arrayTow.map((element)=>{
-        console.log(value === element)
-    })
-    })
+const calsqure =(a)=>{
+    console.log(`the value of a is ${a} and the area is ${squre.area(a)} and the parameter is ${squre.parameters(a)}` )
 }
 
-findCommonArray()
+calsqure(5)
+
+console.log(squre.myDetails("Minhaj","24","Lucknow","aktu"))
+
+
+
--- math.js
+++ math.js
@@ -1,5 +1,9 @@
-// const area =(a)=>(a*a);
-// const parameters=(a)=>(a*4)
+const area =(a)=>(a*a);
+const parameters=(a)=>(a*4)
+const myDetails = (Name,Age,City,College)=>{
+return  `my Name is ${Name}  and I belong to ${City} and I'm ${Age} year old I studied in ${College}`
+}
 
-// module.exports.area = area;
-// module.exports.parameters=parameters
+module.exports.area = area;
+module.exports.parameters=parameters
+module.exports.myDetails=myDetails
