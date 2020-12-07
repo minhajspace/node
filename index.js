55ab67c791c6be4cd35c3d9bc7beca83fd5a1b9c
minhajspace <minhaj.space@gmail.com>
2020-12-07 21:34:44 +0530

    mini project

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 const http = require('http');
+const fs =require('fs');
+const path = require('path')
 const hostName = 'localhost';
 const port = 4000;
 const server =http.createServer((req,res) => {
-   console.log(req.headers);
-   res.statusCode =200 ; 
-   res.setHeader('Content-Type','text/html');
-   res.end('<html><title>server</title><body><h1>server connection successful </h1></body></html>')
+  console.log(`request is coming from  ${req.url} by method  ${req.method}` )
+  if(req.method == 'GET'){
+      let fileUrl ; 
+      if(req.url == '/'){
+          fileUrl = 'index.html'
+      }else {
+          fileUrl = req.url
+      } 
+      var filePath = path.resolve('./public' + fileUrl)
+      const fileExtention = path.extname(filePath)
+      if(fileExtention == '.html'){
+          fs.exists(filePath,(exists)=>{
+              if(!exists){
+                  res.statusCode = 404;
+                  res.setHeader('Content-Type', 'text/html');
+                  res.end('<html><title>server</title><body><h1>error 404 :  ' + fileUrl + ' does not exist   </h1></body></html>')
+              }
+              res.statusCode = 200;
+              res.setHeader('Content-Type', 'text/html');
+             fs.createReadStream(filePath).pipe(res)
+
+          })
+      } else {
+          res.statusCode = 404;
+          res.setHeader('Content-Type', 'text/html');
+          res.end('<html><title>server</title><body><h1>error 404   ' + fileUrl + ' not a html file   </h1></body></html>')
+      }
+  }else {
+
+  }
+
 });
 server.listen(port,hostName)
